5c0018c54169e1210215597eab9f16f5a8c75c09
Surasak Prommarat <prommarats@gmail.com>
2016-02-15 12:33:56 +0700

    fix footer badge

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -185,7 +185,7 @@ AppAsset::register($this);
                              <span id="siteseal" class="pull-right"><script type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=utJxpeZRRfeYzTBhtVOIuZau7RJHWJKDtSP3nm1YsEKPHDlPldoN1r7FBg4m"></script></span>
                         </td>
                         <td width="125">
-                            <a href="https://mixpanel.com/f/partner" rel="nofollow"><img src="//cdn.mxpnl.com/site_media/images/partner/badge_blue.png" alt="Mobile Analytics" /></a>
+                            <a href="https://mixpanel.com/f/partner" rel="nofollow" class="pull-right"><img src="//cdn.mxpnl.com/site_media/images/partner/badge_blue.png" alt="Mobile Analytics" /></a>
                         </td>
                     </tr>
                 </table>

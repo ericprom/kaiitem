6af45cd61da77b5c40c19978c245c911a44229e6
Surasak Prommarat <prommarats@gmail.com>
2016-02-15 12:30:41 +0700

    no message

--- config/db.php
+++ config/db.php
@@ -3,9 +3,9 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=KaiiteMDB',
-    'username' => 'root',
-    'password' => 'root',
-    // 'username' => 'kaiitem',
-    // 'password' => 'P@ssw0rdK@11t3M',
+    // 'username' => 'root',
+    // 'password' => 'root',
+    'username' => 'kaiitem',
+    'password' => 'P@ssw0rdK@11t3M',
     'charset' => 'utf8',
 ];
